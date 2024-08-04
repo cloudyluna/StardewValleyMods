48d8dd49f70041be6b829e13da81b693ac21363e
cloudyluna <luna.cloudberry@proton.me>
2024-08-05 01:12:15 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''
+    '';
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [];
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell rec {
+          buildInputs = [
+            dotnetCorePackages.sdk_8_0_3xx
+            ilspycmd # decompile SDV because there are so many things that
+                     # aren't simply present in public documentation.
+            avalonia-ilspy
+          ];
+
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+
+          shellHook = ''
+            export STARDEW_VALLEY="$HOME/.sdv_dev/drive_c/GOG Games/Stardew Valley/"
+          '';
+        };
+      }
+    );
+}
