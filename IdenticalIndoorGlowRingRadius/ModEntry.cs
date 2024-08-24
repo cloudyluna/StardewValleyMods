5d863682c87c00b8fb59a4a710244a40d6942f47
cloudyluna <luna.cloudberry@proton.me>
2024-08-24 23:40:43 +0800

    Add harmony structure

--- IdenticalIndoorGlowRingRadius/ModEntry.cs
+++ IdenticalIndoorGlowRingRadius/ModEntry.cs
@@ -1,12 +1,18 @@
 ﻿namespace IdenticalIndoorGlowRingRadius;
 
-using StardewValley;
 using StardewModdingAPI;
+using HarmonyLib;
 
 internal class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
     {
-    
+        var harmony = new Harmony(this.ModManifest.UniqueID);
+
+        harmony.Patch(
+            original: AccessTools.Method(typeof(StardewValley.Objects.Ring), nameof(StardewValley.Objects.Ring.onEquip)),
+            prefix: new HarmonyMethod(typeof(RingPatcher), nameof(RingPatcher.OnEquip_Prefix))
+        );
+
     }
 }
---
+++ IdenticalIndoorGlowRingRadius/RingPatcher.cs
@@ -0,0 +1,12 @@
+namespace IdenticalIndoorGlowRingRadius;
+
+using StardewValley;
+
+internal class RingPatcher
+{
+    internal static bool OnEquip_Prefix(Object __instance, Farmer who)
+    {
+
+        return false;
+    }
+}
