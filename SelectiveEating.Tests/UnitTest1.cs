515c5bc47b3ed6758052f0e7ad568777d174fa25
cloudyluna <luna.cloudberry@proton.me>
2024-10-16 22:14:45 +0800

    Add SelectiveEating.Tests

---
+++ SelectiveEating.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace SelectiveEating.Tests;
+
+using NUnit.Framework;
+using SelectiveEating.Model;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(1, Is.EqualTo(5));
+    }
+}
